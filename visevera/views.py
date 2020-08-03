69ac4cd0a2c77f936fad35d11ee5a8609a69bc2b
Александр Нельде <aleksandrnelde@iMac-Aleksandr.local>
2020-08-03 15:36:56 +0600

    Count words

--- visevera/views.py
+++ visevera/views.py
@@ -6,4 +6,6 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reversed_text = user_text[::-1]
-	return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text})
+	count_words = user_text.split()
+	count_words = len(count_words)
+	return render(request,'reverse.html',{'usertext':user_text,'reversedtext':reversed_text,'countwords':count_words})
